97f0dba97e65c345352147eee56fee9db9b9a907
unknown <zidane.abidinur@gmail.com>
2019-01-14 14:16:58 +0700

    first commit

---
+++ application/views/admin/karyawan/list.php
@@ -0,0 +1,9 @@
+<?php
+foreach ($karyawan as $karyawan):
+  echo $karyawan ->id." ";
+  echo $karyawan ->nama." ";
+  echo $karyawan ->alamat." ";
+  echo $karyawan ->telpon."";
+  echo "<br />";
+endforeach;
+ ?>
